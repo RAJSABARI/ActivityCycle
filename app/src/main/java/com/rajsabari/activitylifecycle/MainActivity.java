a6a46294a38affbbb17ec1e9d5fe974fe020e49e
yourstore2002 <yourstorestartup@gmail.com>
2022-12-18 18:46:58 +0530

    Activity|cycle

---
+++ app/src/main/java/com/rajsabari/activitylifecycle/MainActivity.java
@@ -0,0 +1,41 @@
+package com.rajsabari.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String MES="MyMessage";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i(MES)
+
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    private static class string {
+    }
+}
